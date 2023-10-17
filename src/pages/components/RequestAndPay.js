12fc0d603e82e2ef359296f5ced445ad3f0152cf
satyasai69 <ch.satya.sai133@gmail.com>
2023-10-17 19:23:36 +0530

    usestate

--- src/pages/components/RequestAndPay.js
+++ src/pages/components/RequestAndPay.js
@@ -10,15 +10,26 @@ function RequestAndPay (requests, getNameAndBalance) {
 
     const contractadd = "0xb5Ce67ec775BDD14c62BEC526a4DdabCcE54610C"
 
+    const [ PayModal, setPayModal ] = useState(false);
+    const [ requestModal, setrequestModal ] = useState(false);
+  
+
     const { config } = usePrepareContractWrite({
         chanId: goerli.id,
         address: contractadd,
         abi: ABI,
         functionName: "payRequest",
-        args: [0]
+        args: [0],
+        overrider: {
+            value: String(Number(requests["1"][0]*1e18)),
+        }
     })
 
     const { write, data } = useContractWrite(config)
+
+    const { isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    })
 
     const { config: configRequest } = usePrepareContractWrite({
         chanId: goerli.id,
@@ -30,9 +41,36 @@ function RequestAndPay (requests, getNameAndBalance) {
 
     const {write:writeRequest, data:dataRequest} = useContractWrite(configRequest)
 
+    const { isSuccess: isSuccessRequest } = useWaitForTransaction({
+        hash: dataRequest?.hash,
+    })
+
+
+    const showPayModal = () =>{
+        setPayModal(true);
+    }
+
+    const hidePayModal = () => {
+        setPayModal(false);
+    }
+
+    const showRequestPay = () => {
+        setrequestModal(true);
+    }
+
+    const hideRequestModal = () => {
+        setrequestModal(false)
+    }
+
+useEffect(() =>{
+    if(isSuccess || isSuccessRequest) {
+        getNameAndBalance();
+    }
+} , [isSuccess, isSuccessRequest])
+
     return(
         <>
-
+         
         </>
     )
 }
